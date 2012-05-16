49281368144c65097aa5f9e6b9d0f3f0ff503552
Sergey Rezvanov <sergey@rezvanov.info>
2012-05-16 16:47:13 +0600

    Revert "Rewritten admin interface of related products"
    This reverts commit de0ecfdff2d451d311beb28db5438c03eed86f9d.

--- app/controllers/admin/taxons_controller_decorator.rb
+++ app/controllers/admin/taxons_controller_decorator.rb
@@ -1,26 +1,6 @@
 Admin::TaxonsController.class_eval do
-  before_filter :check_json_authenticity, :only => :index
-
-  def index
-    @collection = collection
-    respond_with(@collection) do |format|
-      format.json { render :json => json_data(@collection) }
-    end
-  end
-
   def related
     @taxon = Taxon.find params[:id]
     @relation_types = Taxon.relation_types
-  end
-
-  private
-
-  def collection
-    return [] unless params[:q]
-    Taxon.where(["name like ?", "%#{params[:q]}%"]).uniq
-  end
-
-  def json_data(collection)
-    collection.map { |t| { 'id' => t.id, 'name' => t.name } }.to_json
   end
 end
